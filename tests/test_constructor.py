adbcbcba29dded21b565fc25e01bf28dd00c52bc
Nadya Britvina <nadezhdasmirnova0890@yandex.ru>
2025-12-16 00:48:26 +0300

    Fixed bugs in the test_constructor

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -4,24 +4,20 @@ from selenium.webdriver.support import expected_conditions as EC
 from locators import Locators
 from urls import *
 
-
 class TestConstructorTabs:
-    #Тесты переключения вкладок конструктора
-
     @pytest.mark.parametrize("tab_locator, tab_name", [
-        (*Locators.BUNS_TAB, "Булки"),
-        (*Locators.SAUCES_TAB, "Соусы"),
-        (*Locators.FILLINGS_TAB, "Начинки"),
+        (Locators.BUNS_TAB, "Булки"),
+        (Locators.SAUCES_TAB, "Соусы"),
+        (Locators.FILLINGS_TAB, "Начинки"),
     ])
     def test_constructor_tab_switching(self, logged_user, driver, tab_locator, tab_name):
-        driver = logged_user
-
+        
         driver.find_element(*Locators.CABINET_CONSTRUCTOR).click()
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable(*Locators.BUNS_TAB))
-
-        driver.find_element(tab_locator).click()
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(tab_locator))
-
-        assert driver.find_element(tab_locator).is_displayed(), f"Вкладка '{tab_name}' не отображается"
-        assert driver.find_element(tab_locator).get_attribute("class").find("tab_tab_active") != -1, \
+        
+        driver.find_element(*tab_locator).click() 
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(*tab_locator))
+        
+        assert driver.find_element(*tab_locator).is_displayed(), f"Вкладка '{tab_name}' не отображается"
+        assert "tab_tab_active" in driver.find_element(*tab_locator).get_attribute("class"), \
             f"Вкладка '{tab_name}' не активна"
